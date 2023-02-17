5dfaa52a36583197e34a447b7c3125bec074cc7a
saankaalppp <sankalp18v@gmail.com>
2023-02-18 00:28:58 +0530

    First server

--- first-app.js
+++ first-app.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+console.log('a');
+
+console.log('b');
+
+async function fun1() {
+     setTimeOut(() => console.log('c'), 3000)
+     
+     await function fun2(){
+        setTimeOut(() => console.log('d'), 0)
+    }
+}
+console.log('e');
