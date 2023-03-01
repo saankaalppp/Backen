76d84933aefcfe6d39018e1411fc151a4ff0627a
saankaalppp <sankalp18v@gmail.com>
2023-03-01 14:27:42 +0530

    simple group chat app

---
+++ chat.js
@@ -0,0 +1,47 @@
+const express =require('express');
+const bodyParser=require('body-parser');
+const fs=require('fs')
+
+
+const app= express();
+// app.use(bodyParser.urlencoded())
+  app.use(bodyParser.urlencoded({ extended: false }))
+//   app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+
+
+ app.get("/",(req,res,next)=>{
+    fs.readFile('username.txt',(err,data)=>{
+        if(err){
+            console.log(err)
+            data="No chat exists"
+        }
+    
+        res.send(
+            `${data}  <form onsubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/product" method="POST">
+        <input id="username" type="text" name="username">
+        <input id="message"  type="text" name="message">
+         <button type="submit">add</button>
+        </form>`);
+    })
+ });
+
+app.post('/product',(req,res)=>{
+    console.log(req.body)
+    console.log(req.body.username);
+    console.log(req.body.message);
+    fs.writeFile("username.txt",` ${req.body.username}: ${req.body.message} `,{flag:"a"} ,(err)=>
+        err ? console.log(err) : res.redirect('/')
+    );
+});
+
+app.get('/login',(req,res)=>{
+    res.send(`<form  onsubmit="localStorage.setItem('username', document.getElementById('username').value)" 
+    action="/product" method="POST">
+    <input id="username" type="text" name="username">
+    <input id="message"  type="text" name="message">
+    <button type="submit">add</button>
+    </form>`);
+})
+
+app.listen(4000)
