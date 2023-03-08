68dd9a747533e5202a958e6755785fb0f52f9554
saankaalppp <sankalp18v@gmail.com>
2023-03-08 21:17:19 +0530

    Models

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,4 @@
-const products = [];
-
+const Product=require("../models/product")
 exports.getAddProduct=(req, res, next) => {
     res.render('add-product', {
       pageTitle: 'Add Product',
@@ -11,12 +10,14 @@ exports.getAddProduct=(req, res, next) => {
   }
 
 exports.postAddProduct= (req, res, next) => {
-    products.push({ title: req.body.title });
-    res.redirect('/');
+    const product= new Product(req.body.title)
+    product.save();
+     res.redirect('/');
   }
 
 
  exports.getProducts= (req, res, next) => {
+  Product.fetchall(products=>{
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
@@ -25,6 +26,7 @@ exports.postAddProduct= (req, res, next) => {
       activeShop: true,
       productCSS: true
     });
+  })
   }
 
   
