de73e93663b8ee2e66635136d5bf9f3e031b09b6
saankaalppp <sankalp18v@gmail.com>
2023-03-12 00:57:23 +0530

    Dynamic Routes 2 (POST)

--- hello.js
+++ hello.js
@@ -3,9 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const errorController = require('./controllers/error');
 
-const errorController=require('./controllers/error')
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,8 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
 app.use(errorController.get404);
 
 app.listen(3000);
-
